89ae1eedfd017e91f3cd4f89cc45d329e046d554
Babita <Babitabhati26@gmail.com>
2024-10-04 06:35:52 +0530

    commit and Push

---
+++ src/Oct/ex_03102024/Lab004.java
@@ -0,0 +1,10 @@
+package Oct.ex_03102024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println(10-1);
+        System.out.println(10*2);
+        System.out.println(10/3);
+        System.out.println(10+4);
+    }
+}
