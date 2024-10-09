6bc88ac615d8cac515cbe8ba93cdf8754b62eed8
Babita <Babitabhati26@gmail.com>
2024-10-09 12:51:31 +0530

    commit and Push

---
+++ src/Oct/ex_08102024/Lab00810_variable.java
@@ -0,0 +1,10 @@
+package Oct.ex_08102024;
+
+public class Lab00810_variable {
+    public static void main(String[] args) {
+
+        //Variables
+        int a=65;
+
+    }
+}
---
+++ src/Oct/ex_08102024/Lab_table.java
@@ -0,0 +1,27 @@
+package Oct.ex_08102024;
+
+public class Lab_table {
+    public static void main(String[] args) {
+        int i=9;
+        System.out.printf("%d * %d = %d ", i, 1, i);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 2, i * 2);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 3, i * 3);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 4, i * 4);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 5, i * 5);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 6, i * 6);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 7, i * 7);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 8, i * 8);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 9, i * 9);
+        System.out.println();
+        System.out.printf("%d * %d = %d ", i, 10, i * 10);
+        System.out.println();
+    }
+}
