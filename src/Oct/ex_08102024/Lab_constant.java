edf868f85dc2a11b4d957bb4f8380dd5d51ab725
Babita <Babitabhati26@gmail.com>
2024-10-09 13:08:44 +0530

    commit and Push

---
+++ src/Oct/ex_08102024/Lab_constant.java
@@ -0,0 +1,15 @@
+package Oct.ex_08102024;
+
+public class Lab_constant {
+     public static int i=10;
+    //final int j= 90;
+     final static String s="Radha";
+
+    public static void main(String[] args) {
+        System.out.println("Number of Employee ="+ i);
+        System.out.println("Name of Employee ="+ s);
+
+
+
+    }
+}
