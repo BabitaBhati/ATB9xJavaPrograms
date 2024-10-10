773bdbbbaf2c3f30a85a039acb74d3262b96f5b0
Babita <Babitabhati26@gmail.com>
2024-10-10 22:04:00 +0530

    commit and Push

---
+++ src/Oct/ex_1010_Constant/Lab1010_final.java
@@ -0,0 +1,18 @@
+package Oct.ex_1010_Constant;
+
+public class Lab1010_final {
+
+
+
+    public static void main(String[] args) {
+
+
+
+        final int n = 10;
+        final String s = "Babita";
+        //If you try to change the value - `java: cannot assign a value to final variable PI`
+        // n= 90;
+
+        System.out.println(n);
+    }
+}
---
+++ src/Oct/ex_Literals_1010/Lab_1010_AllLiterals.java
@@ -0,0 +1,33 @@
+package Oct.ex_Literals_1010;
+
+public class Lab_1010_AllLiterals {
+    public static void main(String[] args) {
+
+
+    int count = 987;
+//Floating-point literal
+    float floatVal = 4534.99f;
+//Double-precision floating-point literal
+    double cost = 19765.567;
+//Hexadecimal literal
+    int hexaVal = 0x7e4;
+//Binary literal
+    int binary = 0b11010;
+ //Boolean literal
+    boolean boolVal = true;
+  //Octal literal
+    int octalVal = 067;
+
+    // String literal
+    String str1 = "My name is Snehal";
+    //String containing numbers
+    String str2 = "14567";
+
+    //.Character Literal
+    char ch1 = 'A';
+
+
+    //Escape sequence
+    char ch2 = 1456;
+}
+}
