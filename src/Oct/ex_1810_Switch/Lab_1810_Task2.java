b4e8ea4cb1f789b0504d76c0dfb8b95777b39e78
Babita <Babitabhati26@gmail.com>
2024-10-18 18:12:35 +0530

    commit and Push

---
+++ src/Oct/ex_1810_Switch/Lab_1810_Task2.java
@@ -0,0 +1,52 @@
+package Oct.ex_1810_Switch;
+
+import java.util.Scanner;
+
+public class Lab_1810_Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please any number between 1 to 12 to display the corresponding month:");
+        int month = sc.nextInt();
+        switch (month){
+            case 1:
+                System.out.println("Jan");
+                break;
+            case 2:
+                System.out.println("Feb");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("Aug");
+                break;
+            case 9:
+                System.out.println("Sept");
+                break;
+            case 10:
+                System.out.println("Oct");
+                break;
+            case 11:
+                System.out.println("Nov");
+                break;
+            case 12:
+                System.out.println("Dec");
+                break;
+            default:
+                System.out.println("You selected invalid number.");
+                sc.close();
+        }
+    }
+}
